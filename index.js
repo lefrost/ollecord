3165ffabb5bc8ac513dcdedbaa7424078ac6dae4
Lé <elcasaboxi@gmail.com>
2021-10-22 16:48:12 +0800

    added `express`

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+const express = require(`express`);
+const app = express();
 const { Client, Intents } = require("discord.js");
 const token = process.env.TOKEN;
 // const clientId = process.env.CLIENT_ID;git
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let port = process.env.PORT || 3000;
+
+app.listen(port, async () => {
+  console.log(`Up on http://localhost:${port}`);
+});
 
 client.once("ready", async () => {
   console.log("Ready!");
